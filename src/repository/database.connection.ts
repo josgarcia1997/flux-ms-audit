5242501aed4d801325a2116718060681c73d53c3
DianaPraxis <d2.garzon@praxisglobe.net>
2026-04-08 22:39:55 -0500

    Update database.connection.ts

--- src/repository/database.connection.ts
+++ src/repository/database.connection.ts
@@ -17,7 +17,7 @@ export class DatabaseConnection implements OnModuleInit, OnModuleDestroy {
             user: dbConfig.user,
             password: dbConfig.password,
             database: dbConfig.name,
-            ssl: dbConfig.ssl ? { require: true, rejectUnauthorized: false } : false,
+            ssl: dbConfig.ssl ? { rejectUnauthorized: false } : false,
             max: 20,
             idleTimeoutMillis: 30000,
             connectionTimeoutMillis: 2000,
